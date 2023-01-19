5574c1df003457f5ba845e748924943d07ee3d3e
Deepak Dutta <coder.deepakyt@gmail.com>
2023-01-18 23:48:20 -0700

    Add mail user_name and password for testing

--- config/mail.config.js
+++ config/mail.config.js
@@ -4,7 +4,7 @@ import envConfig from "../config/env.config.js";
 // create reusable transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
   host: envConfig.MAIL_HOST,
-  port: envConfig.MAIL_PORT,
+  service: "gmail",
   secure: false,
   auth: {
     user: envConfig.MAIL_USERNAME,
